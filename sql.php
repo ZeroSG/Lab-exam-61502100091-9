b0baadc60a9d6be4bed814916e663dd4f791f7e8
Your Name <you@example.com>
2021-02-10 08:32:47 +0700

    1.1

---
+++ sql.php
@@ -0,0 +1,11 @@
+<?php
+$conn = new mysqli("localhost","root","","61502100091-9");
+ 
+// Check connection
+if ($conn -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $conn -> connect_error;
+  exit();
+}else {
+     echo "เชื่อม MySQLได้";
+}
+?>
